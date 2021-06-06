5047effe35a972fe8eea0182929df0536a46aa19
mike tinn <miketinn01@gmail.com>
2021-06-07 10:07:38 +1200

    to end of 4/ working with static members

---
+++ Book.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp8
+{
+    class Book
+    {
+        public Book(string name)       //
+        {
+            grades = new List<double>();
+            this.name = name;       // this
+        }
+        public void AddGrade(double grade)      //public is an access modifier, it controls access to the member of the class. 
+        {
+                grades.Add(grade);
+        }
+        private List<double> grades;
+        private string name;    // creates a private field name
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,48 +3,34 @@ using System.Collections.Generic;
 
 namespace ConsoleApp8
 {
+
     class Program
     {
-
-
-        static void Main(string[] args)
+        static void Main(string[] args)     //
         {
-            List<double> myList = new List<double>();   // a List is a dynamic version of an array
+            var Book1 = new Book("Scotts grade book");
+            Book1.AddGrade(89.1);
+            Book1.AddGrade(90.5);
+            Book1.AddGrade(77.5);
 
-            var input = "";
-            while (input != "x")
-            {
-                Console.WriteLine("enter the a number");
-                input = Console.ReadLine();
-                if (input != "x")
+         var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+         grades.Add(56.1);
+
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (var number in grades)       // do this for every number in the array
                 {
-                    myList.Add(double.Parse(input)); 
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
                 }
-            }          
-
-            var sum = 0.0;             // declare variable to add sum of the array
-
-            foreach (var number in myList)       // do this for every number in the array
-            {
-                sum += number;  // add to the previous total
-            }
-
-            Console.WriteLine($"\nThe sum of the numbers is {sum}");   // write to the screen
-
-            Console.WriteLine(myList.Count);        // returns the value of elemets in a list
-            var listAve = sum/ myList.Count;     // returns the average of elemets in a list
-            Console.WriteLine($"\nThe average of the numbers is {listAve:N2}");   
-
-            Console.ReadKey();
-
-            //hello
-            // change #2
-            //c3
-
-      
-
+            result /= grades.Count;
+            Console.WriteLine($"\nThe lowest grade is {lowGrade:N2}");
+            Console.WriteLine($"\nThe highest grade is {highGrade:N2}");
+            Console.WriteLine($"\nThe average grade is {result:N2}");
         }
-
         private static void DoAdd(string input)
         {
             throw new NotImplementedException();
