255e5b628ad54e18509921fcb89ea9ce3716d0a2
mike tinn <miketinn01@gmail.com>
2021-06-07 11:58:30 +1200

    modifications to class Book to calculate stats and write to the console

--- Book.cs
+++ Book.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+
 
 namespace ConsoleApp8
 {
     class Book
     {
-        public Book(string name)       //
+        public Book(string name)       // requires a name when invoke constructor
         {
             grades = new List<double>();
-            this.name = name;       // this
+            this.name = name;       // this is used when the field has the same name a parameter
         }
+        // below creates the method AddGrade
         public void AddGrade(double grade)      //public is an access modifier, it controls access to the member of the class. 
         {
-                grades.Add(grade);
+                grades.Add(grade);      
         }
+        public void CalcStats()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;        //inicialize variable to the smallest value possible
+            var lowGrade = double.MaxValue;         //inicialize variable to the highest value possible
+            foreach (var number in grades)       // do this for every number in the array
+            {
+                highGrade = Math.Max(number, highGrade);        //returns the highest of the 2 numbers
+                lowGrade = Math.Min(number, lowGrade);          //returns the lowest of the 2 numbers
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"\nStatistics for {name}");
+            Console.WriteLine($"\nThe lowest grade is {lowGrade}");
+            Console.WriteLine($"\nThe highest grade is {highGrade}");
+            Console.WriteLine($"\nThe average grade is {result:N2}");
+        }
+
         private List<double> grades;
         private string name;    // creates a private field name
     }
--- Program.cs
+++ Program.cs
@@ -8,28 +8,11 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)     //
         {
-            var Book1 = new Book("Scotts grade book");
-            Book1.AddGrade(89.1);
+            var Book1 = new Book("Scotts grade book");      //piece of state "Scotts grade book" is encapsulated inside the book object
+            Book1.AddGrade(89.1);       
             Book1.AddGrade(90.5);
             Book1.AddGrade(77.5);
-
-         var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-         grades.Add(56.1);
-
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-
-            foreach (var number in grades)       // do this for every number in the array
-                {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-                result += number;
-                }
-            result /= grades.Count;
-            Console.WriteLine($"\nThe lowest grade is {lowGrade:N2}");
-            Console.WriteLine($"\nThe highest grade is {highGrade:N2}");
-            Console.WriteLine($"\nThe average grade is {result:N2}");
+            Book1.CalcStats();
         }
         private static void DoAdd(string input)
         {
