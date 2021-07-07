e80ad57e253a8ffa93d542136b42671f20ad8eb2
mike tinn <miketinn01@gmail.com>
2021-07-07 20:42:34 +1200

    7 jul 21

--- Book.cs
+++ Book.cs
@@ -11,7 +11,6 @@ namespace GradeBook_v3
 
         private List<double> grades;    // creates a private List of type double with name grades
         
-
         public Book(string name)       // requires a name when invoke constructor
         {
             grades = new List<double>(); // this code snippet creates a list of type double & total number of items it can hold is left open.
@@ -22,10 +21,38 @@ namespace GradeBook_v3
        public string Name { get; set; }              //{ get; set; } are referred as {{c1::property accessors}} and are auto - implemented properties
                                                      // by convention public members like Name (or public members, fields or other types) must have Uppercase N
 
+        // below creates the method AddLetterGrade
+        public void AddLetterGrade(char letter)      //
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);   // adds the grade in place of the letter to the method AddGrade
+                    break;
+                case 'B':
+                    AddGrade(80);   // adds the grade in place of the letter to the method AddGrade
+                    break;
+                case 'C':
+                    AddGrade(70);   // adds the grade in place of the letter to the method AddGrade
+                    break;
+                default:
+                    AddGrade(0);   // adds the grade 0 in place of the any other letter the method AddGrade
+                    break;
+            }
+        }
+
         // below creates the method AddGrade
         public void AddGrade(double grade)      //public is an access modifier, it controls access to the member of the class. 
         {
-            grades.Add(grade);
+            if(grade <= 100 && grade >= 0 )
+                {
+                grades.Add(grade);
+                }
+            else
+            {
+                //Console.WriteLine("Invalid value for grade");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
         }
 
         public Statistics GetStats()    // this is a method
@@ -36,13 +63,38 @@ namespace GradeBook_v3
             result.Low = double.MaxValue;         //inicialize variable to the highest value possible
             //var sumOfGrades = 0.00;
 
-            foreach (var grade in grades)       // do this for every number in the array
+           // foreach (var grade in grades)       // do this for every number in the array          previous loop style
+           for ( var index = 0; index < grades.Count; index +=1 )
             {
-                result.Low = Math.Min(grade, result.Low);      //returns the lowest of the 2 numbers
-                result.High = Math.Max(grade, result.High);      //returns the highest of the 2 numbers
-                result.Average+= grade;
+                result.Low = Math.Min(grades[index], result.Low);      //returns the lowest of the 2 numbers
+                result.High = Math.Max(grades[index], result.High);      //returns the highest of the 2 numbers
+                result.Average+= grades[index];
             }
             result.Average /= grades.Count; // DT I'd call this ave or avg or average - call it what it is.  Is part of something called 'Clean Coding' (started by 'Uncle Bob') for when you bored and feel like googling.
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+
+            }
             return result;      // returns newly created object 
         }
     }
--- Program.cs
+++ Program.cs
@@ -8,15 +8,45 @@ namespace GradeBook_v3
         static void Main(string[] args) 
         {
             var Book1 = new Book("Scotts grade book");      //piece of state "Scotts grade book" is encapsulated inside the book object
-            Book1.AddGrade(89.1);
-            Book1.AddGrade(90.5);
-            Book1.AddGrade(77.5);
+
+            //loop to collect grades from concole until user enters letter q
+            while(true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+                if(input == "q")
+                {
+                    break;
+                }
+                try   // 1st try to do the code below... if it fails it will jumpt to the catch statement
+                {
+                    var grade = Convert.ToDouble(input);
+                    //var grade = Double.Parse(input);  // this line does the same as the above 
+
+                    Book1.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                // catch(Exception ex) this exception will catch everything which isn't always the right thing
+                // we want to be specific about which errors to catch and how to handle them
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    // allows you to do something regardless of chaos thats happening above
+                }
+            }
 
             var StatisticsClass = Book1.GetStats();
             //Console.WriteLine($"\nStatistics for {Book.Name}");
             Console.WriteLine($"\nThe lowest grade is {StatisticsClass.Low}");
             Console.WriteLine($"\nThe highest grade is {StatisticsClass.High}");
             Console.WriteLine($"\nThe average grade is {StatisticsClass.Average:N2}");
+            Console.WriteLine($"\nThe letter grade is {StatisticsClass.Letter}");
         }
         private static void DoAdd(string input)
         {
